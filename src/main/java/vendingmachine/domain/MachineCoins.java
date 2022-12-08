4f54bdcceb76bf16ea69dd9e28c7f3533c8c8afd
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-08 16:27:50 +0900

    refactor(MachineCoins): LinkedHashMap으로 변경

--- src/main/java/vendingmachine/domain/MachineCoins.java
+++ src/main/java/vendingmachine/domain/MachineCoins.java
@@ -1,14 +1,24 @@
 package vendingmachine.domain;
 
 import vendingmachine.enums.Coin;
+import vendingmachine.view.OutputView;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class MachineCoins {
-    private Map<Coin, Integer> coins;
+    private LinkedHashMap<Coin, Integer> coins;
     private RandomCoinMaker randomCoinMaker = new RandomCoinMaker();
 
     public MachineCoins(int money) {
         coins = randomCoinMaker.createRandomCoins(money);
+    }
+
+    public Map<Coin, Integer> getCoins() {
+        return coins;
+    }
+
+    public static void main(String[] args) {
+        OutputView.printVendingMachineCoins(new MachineCoins(450).getCoins());
     }
 }
