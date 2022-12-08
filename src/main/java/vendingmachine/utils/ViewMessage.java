2a04b21f0d34e98990240a0ed8792936706cbce1
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-08 14:37:38 +0900

    feat(ViewMessage): 입력시 텍스트 정의

---
+++ src/main/java/vendingmachine/utils/ViewMessage.java
@@ -0,0 +1,8 @@
+package vendingmachine.utils;
+
+public class ViewMessage {
+    public static final String INPUT_VENDING_MACHINE_MONEY = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    public static final String INPUT_NAME_COST_QUANTITY_OF_PRODUCT = "상품명과 가격, 수량을 입력해 주세요.";
+    public static final String INPUT_CUSTOMER_MONEY = "투입 금액을 입력해 주세요.";
+    public static final String INPUT_PRODUCT_NAME_TO_BUY = "구매할 상품명을 입력해 주세요.";
+}
