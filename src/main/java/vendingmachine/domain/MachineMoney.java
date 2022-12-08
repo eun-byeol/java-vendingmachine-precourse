cf0eb88e709865050981c7b160754896a0c32213
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-08 17:13:27 +0900

    refactor(MachineMoney): 보유 금액 long형에서 int형으로 수정

--- src/main/java/vendingmachine/domain/MachineMoney.java
+++ src/main/java/vendingmachine/domain/MachineMoney.java
@@ -3,31 +3,31 @@ package vendingmachine.domain;
 import static vendingmachine.utils.ErrorMassage.*;
 
 public class MachineMoney {
-    private final long machineMoney;
-    private static final long UNIT_AMOUNT = 10L;
+    private final int machineMoney;
+    private static final int UNIT_AMOUNT = 10;
 
     public MachineMoney(String inputMoney) {
-        Long money = validateLong(inputMoney);
+        int money = validateInteger(inputMoney);
         validatePositiveNumber(money);
         validateDividedTen(money);
         this.machineMoney = money;
     }
 
-    private Long validateLong(String inputMoney) {
+    private int validateInteger(String inputMoney) {
         try {
-            return Long.parseLong(inputMoney);
+            return Integer.parseInt(inputMoney);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(MACHINE_MONEY_NOT_LONG);
+            throw new IllegalArgumentException(MACHINE_MONEY_NOT_INTEGER);
         }
     }
 
-    private void validatePositiveNumber(long money) {
+    private void validatePositiveNumber(int money) {
         if (money <= 0) {
             throw new IllegalArgumentException(MACHINE_MONEY_NOT_POSITIVE_NUMBER);
         }
     }
 
-    private void validateDividedTen(long money) {
+    private void validateDividedTen(int money) {
         if (money % UNIT_AMOUNT != 0) {
             throw new IllegalArgumentException(MACHINE_MONEY_COULD_BE_DIVIDED_BY_TEN);
         }
