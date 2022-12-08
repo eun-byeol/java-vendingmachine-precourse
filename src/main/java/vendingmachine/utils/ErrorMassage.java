a9696764ecf6d920ec34d47be04ed3bd842509ca
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-08 18:57:32 +0900

    feat(ErrorMassage): 구매할 상품명 입력 오류 메시지 추가

--- src/main/java/vendingmachine/utils/ErrorMassage.java
+++ src/main/java/vendingmachine/utils/ErrorMassage.java
@@ -13,4 +13,8 @@ public class ErrorMassage {
     public static final String PRODUCT_PRICE_RANGE_HAS_TO_OVER_100 = ERROR_TAG + "상품 가격은 100원 이상이어야 합니다.";
     public static final String PRODUCT_PRICE_COULD_BE_DIVIDED_BY_TEN = ERROR_TAG + "상품 가격은 10원으로 나누어 떨어져야 합니다.";
     public static final String PRODUCT_QUANTITY_NOT_POSITIVE_NUMBER = ERROR_TAG + "상품 수량은 최소 1개 이상이어야 합니다.";
+
+    public static final String PRODUCT_IS_OUT_OF_STOCK = ERROR_TAG + "재고가 없습니다.";
+    public static final String NOT_ENOUGH_MONEY = ERROR_TAG + "투입 금액이 부족합니다.";
+    public static final String PRODUCT_NAME_IS_NOT_VALID = ERROR_TAG + "등록된 상품이 아닙니다.";
 }
